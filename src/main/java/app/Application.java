2d09e247d361d88f92fde627836dd60306693a8f
alexbalak21 <alex.balak@outlook.com>
2026-06-18 22:00:13 +0200

    Working App

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootJwtAuthApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootJwtAuthApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
